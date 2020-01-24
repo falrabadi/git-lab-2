38698060f5aefcb02e4a50dd04614026368cd6b8
Fadi Alrabadi <fa979119@ohio.edu>
2020-01-24 15:02:26 -0500

    git-lab-program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Fadi Alrabadi
+ *        Date: 1-24-2020
+ * Description: Lab 2 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+
+    return 0;
+}// main
